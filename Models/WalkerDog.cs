8d97bfdc4be9592b903e3b5f9bbc54e830e613af
Rhmuse <rhett.muse123@gmail.com>
2023-08-24 15:24:29 -0500

    define models and add dummy data

---
+++ Models/WalkerDog.cs
@@ -0,0 +1,8 @@
+namespace DeShawnsDogWalking.Models;
+
+public class WalkerDog
+{
+    public int Id { get; set; }
+    public int WalkerId { get; set; }
+    public int DogId { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,44 @@
+using DeShawnsDogWalking.Models;
+
+List<Walker> walkers = new List<Walker>
+{
+    new Walker { Id = 1, Name = "John" },
+    new Walker { Id = 2, Name = "Emily" },
+    new Walker { Id = 3, Name = "Michael" }
+};
+
+List<City> cities = new List<City>
+{
+    new City { Id = 1, Name = "New York" },
+    new City { Id = 2, Name = "Los Angeles" },
+    new City { Id = 3, Name = "Chicago" }
+};
+
+List<Dog> dogs = new List<Dog>
+{
+    new Dog { Id = 1, Name = "Max", CityId = 1, WalkerId = 1 },
+    new Dog { Id = 2, Name = "Bella", CityId = 2, WalkerId = 2 },
+    new Dog { Id = 3, Name = "Charlie", CityId = 1, WalkerId = 1 },
+    new Dog { Id = 4, Name = "Lucy", CityId = 3, WalkerId = 3 }
+};
+
+List<WalkerDog> walkerDogs = new List<WalkerDog>
+{
+    new WalkerDog { Id = 1, WalkerId = 1, DogId = 1 },
+    new WalkerDog { Id = 2, WalkerId = 2, DogId = 2 },
+    new WalkerDog { Id = 3, WalkerId = 1, DogId = 3 },
+    new WalkerDog { Id = 4, WalkerId = 3, DogId = 4 }
+};
+
+List<WalkerCity> walkerCities = new List<WalkerCity>
+{
+    new WalkerCity { Id = 1, CityId = 1, WalkerId = 1 },
+    new WalkerCity { Id = 2, CityId = 2, WalkerId = 2 },
+    new WalkerCity { Id = 3, CityId = 1, WalkerId = 3 },
+    new WalkerCity { Id = 4, CityId = 3, WalkerId = 1 }
+};
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
