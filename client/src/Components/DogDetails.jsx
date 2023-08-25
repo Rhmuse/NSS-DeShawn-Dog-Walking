c66ffb7abf1191d25fe7e261106700c73b99e658
Rhmuse <rhett.muse123@gmail.com>
2023-08-25 11:21:46 -0500

    add dog details

---
+++ client/src/Components/DogDetails.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getDogById } from '../apiManager';
+
+const DogDetails = () => {
+	const { id } = useParams();
+	const [dog, setDog] = useState({
+		name: '',
+		id: 0,
+		walkerId: 0,
+		walker: {},
+	});
+
+	useEffect(() => {
+		getDogById(id).then((dog) => {
+			setDog(dog);
+		});
+	}, [id]);
+
+	return (
+		<>
+			<h2>{dog.name}</h2>
+			<p>Walker: {dog.walker?.name}</p>
+			<p>City: {dog.city?.name}</p>
+		</>
+	);
+};
+
+export default DogDetails;
